e34dea7de17cbc91537ce9bc82efc17d432ad40a
tobechukwu.n.n@gmail.com <*nuk%Nai#dyC7b2XkuL@ZT#A&&y$MF>
2021-08-04 03:55:28 +0100

    Implemented fuzzy search method.

--- Film Rename Bot.py
+++ Film Rename Bot.py
@@ -1,9 +1,10 @@
 import re
 import os
 import time
+import Levenshtein
 
 # Creator: Tobechukwu N. Njoku
-# Last Update: 2021/07/31
+# Last Update: 2021/08/04
 
 regex_audio = re.compile(r'.*([2-7] [0-2][ ])')  # Using Regular expression to fix audio
 regex_resolutions = re.compile(r'.*([1-2][0-9]{3}[p] )')  # Using Regular expression to find the resolution
@@ -14,10 +15,8 @@ regex_anomaly = re.compile(r'.*([)] (.*.) [\[])')  # Using Regular expression to
 def movie_rename(file_name):
     renaming_history_file_name = "Film Rename Bot History.txt"
     special_characters = (") [", "(", ")", "[", "]", ":", "_", ".", " - ", "-", "  ")
-    distorted_data = {" A ": " a ", " Of ": " of ", " And ": " and ", " The ": " the ", "X26": "x26", "H26": "x26",
-                      "Im ": "I'm ", "St ": "St. ", "WEB DL": "Web-DL", "WEBRip": "WebRip", "BluRay": "Blu-ray",
-                      "Blu ray": "Blu-ray", "bluray": "Blu-ray", "eztv re": "eztv.re", "a K a": "A.K.A.",
-                      "10-bit": "10bit"}
+    distorted_data = {" A ": " a ", " Of ": " of ", " And ": " and ", " The ": " the ", "a K a": "A.K.A.",
+                      "WEB DL": "Web-DL"}
     # opening_bracket = special_characters[3]
     # closing_bracket = special_characters[4]
 
@@ -31,15 +30,15 @@ def movie_rename(file_name):
 
     print("Input: " + file_name)
 
-    file_name, file_extension = os.path.splitext(file_name)  # Remove and save file extension.
+    file_name, file_extension = os.path.splitext(file_name)     # Remove and save the file extension.
 
-    for x in special_characters:  # Replace certain characters with spaces.
+    for x in special_characters:        # Replace certain characters with spaces.
         file_name = file_name.replace(x, " ")
 
-    for key, value in distorted_data.items():  # Replace broken metadata
+    for key, value in distorted_data.items():       # Replace broken metadata
         file_name = file_name.replace(key, value)
 
-    audio_found = re.search(regex_audio, file_name)  # Search for broken audio format in file name.
+    audio_found = re.search(regex_audio, file_name)     # Search for broken audio format in file name.
     if audio_found:  # Fixing the naming of audio if it is found in a broken state.
         sound_format = audio_found.group(1).strip().replace(" ", ".")
         print("Found Sound Format: " + sound_format)
@@ -47,7 +46,7 @@ def movie_rename(file_name):
     else:
         print("Error: Sound format not found!")
 
-    resolution_found = re.search(regex_resolutions, file_name)  # Search for resolution item in file name.
+    resolution_found = re.search(regex_resolutions, file_name)      # Search for resolution item in file name.
     if resolution_found:  # Putting a square bracket in front of the resolution if it is found.
         resolution = resolution_found.group(1).strip()
         print("Found Resolution: " + resolution)
@@ -55,38 +54,35 @@ def movie_rename(file_name):
     else:
         print("Error: Resolution not found!")
 
-    year_found = re.search(regex_year, file_name)  # Search for year in file name.
-    if year_found:  # Padding the year if it's found with parentheses.
+    year_found = re.search(regex_year, file_name)       # Search for year in file name.
+    if year_found:      # Padding the year if it's found with parentheses.
         year = year_found.group(1).strip()
         print("Found Year: " + year)
         file_name = file_name.replace(year, " (" + year + ")")
     else:
         print("Error: Year not found!")
 
-    anomaly_found = re.search(regex_anomaly, file_name)  # Search for misplaced item in file name.
-    if anomaly_found:  # Moving any anomalies found between the year and start of metadata to the end.
+    anomaly_found = re.search(regex_anomaly, file_name)     # Search for misplaced item in file name.
+    if anomaly_found:       # Moving any anomalies found between the year and start of metadata to the end.
         anomaly = anomaly_found.group(2)        # Group 1 = ")x[
-        print("Found Anomaly: " + anomaly)        # Group 2 = "x"
+        print("Found Anomaly: " + anomaly)      # Group 2 = "x"
         # print("Group 1 = " + anomaly_found.group(1) + "\nGroup 2 = " + anomaly_found.group(2))        # Print info.
         file_name = file_name.replace(anomaly_found.group(1), ") [").replace("]", "") + " " + anomaly
     else:
-        print("Error: Anomaly not found!")
+        print("Warning: Anomaly not found!")
 
-    while file_name[-1] == " ":  # Remove trailing whitespaces.
+    file_name = re.sub(r'(\s\s+)', ' ', file_name)        # Removing double spaces in the file name.
+
+    while file_name[-1] == " ":     # Remove trailing whitespaces.
         if file_name[-1].isalnum():
             break
         else:
             file_name = file_name[:-1]
 
-    while "  " in file_name:  # Removing double spaces in the file name.
-        if "  " not in file_name:
-            break
-        else:
-            file_name = file_name.replace("  ", " ")
+    file_name = levenshtein_fuzzy_search(file_name)
 
-    file_name = file_name.replace(")[", ") [")  # Adjusting the spacing between year and metadata.
-
-    file_name += "]" + file_extension  # Re-attach the file extension to the finished name.
+    file_name = file_name.replace(")[", ") [")      # Adjusting the spacing between year and metadata.
+    file_name += "]" + file_extension       # Re-attach the file extension to the finished name.
 
     # open_bracket_index = file_name.index(opening_bracket)
     # closing_bracket_index = file_name.index(closing_bracket)
@@ -94,6 +90,39 @@ def movie_rename(file_name):
     file.write("Output: " + file_name + " \n")
     file.close()
     print("Finished: " + file_name + "\n")
+    return file_name
+
+
+def levenshtein_fuzzy_search(file_name):
+    fuzzy_search_items = ['x265', 'I\'m ', 'St. ', '10bit', 'HDR', 'Web-DL', 'WebRip', 'Blu-ray', 'eztv.re']
+
+    input_data = file_name.split()      # Putting the file name into a list.
+
+    # print("Input Data", end=": ")
+    # print(input_data)
+    # print("\n")
+
+    for index, item in enumerate(input_data):
+        for index2, item2 in enumerate(fuzzy_search_items):     # Nested for loop, to match all indexes between two lists.
+            levenshtein_ratio = Levenshtein.ratio(input_data[index].lower(), fuzzy_search_items[index2].lower())
+
+            if levenshtein_ratio > 0.7:     # Replace if the code if the match is good.
+                print("Input", end=": ")
+                print(input_data[index])
+                input_data[index] = fuzzy_search_items[index2]
+                print("Levenshtein Ratio", end=": ")
+                print(levenshtein_ratio)
+                print("Output", end=": ")
+                print(input_data[index])
+                print("\n")
+
+    # print("Output Data", end=": ")
+    # print(input_data)
+    space = " "
+    output_data = space.join(input_data)
+    # print(output_data)
+
+    file_name = output_data
     return file_name
 
 
